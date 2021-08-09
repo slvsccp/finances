ed5fff4419072d42209372620267c5774bf6303a
Matheus silva <teus_26@hotmail.com>
2021-08-09 18:50:39 -0300

    transactions

--- js/js.js
+++ js/js.js
@@ -10,3 +10,56 @@ const Modal = {
     document.querySelector('.modal-overlay').classList.remove('active')
   }
 }
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '30/07/2021'
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '30/07/2021'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '30/07/2021'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    // somar as entradas
+  },
+  expenses() {
+    // somar as saídas
+  },
+  total() {
+    // entradas - saídas
+  }
+}
+
+const DOM = {
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction()
+  },
+  innerHTMLTransaction() {
+    const html = `
+    <td class="description">Luz</td>
+    <td class="expense">- R$500,00</td>
+    <td class="date">30/07/2021</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>`
+
+    return html
+  }
+}
+
+DOM.addTransaction(transactions[0])
